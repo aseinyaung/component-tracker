1e4d870bbf4864bebb4a00936cfd828b032fc561
Aung Zwe Htet Kyaw <azh.kyaw@gmail.com>
2013-02-16 22:24:08 +1000

    Add database connection configuration settings

---
+++ includes/config.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+$db_config = [
+    'driver'   => 'mysql',
+    'host'     => 'localhost',
+    'dbname'   => 'component_tracker',
+    'username' => 'development_user',
+    'password' => 'development_pass',
+    'charset'  => 'utf8'
+];
